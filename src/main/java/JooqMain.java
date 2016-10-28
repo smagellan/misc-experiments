8e07a3e89f849c270153834e3b959a3653e0a569
Vladimir Vishnevsky <smagellan@gmail.com>
2016-10-28 10:16:17 +0300

    jooq over ClickHouse experiments

--- src/main/java/JooqMain.java
+++ src/main/java/JooqMain.java
@@ -1,31 +1,42 @@
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.ZoneOffset;
-import java.util.Collection;
-import java.util.Iterator;
+
+import javax.sql.DataSource;
 
 import com.google.common.primitives.Ints;
 import org.jooq.DSLContext;
 import org.jooq.Field;
-import org.jooq.Insert;
-import org.jooq.InsertOnDuplicateSetMoreStep;
-import org.jooq.InsertOnDuplicateStep;
-import org.jooq.InsertValuesStepN;
-import org.jooq.Record;
-import org.jooq.Select;
-import org.jooq.Table;
+import org.jooq.Name;
+import org.jooq.SQLDialect;
+import org.jooq.conf.RenderNameStyle;
+import org.jooq.conf.Settings;
 import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultDSLContext;
 import org.jooq.types.DayToSecond;
-import org.jooq.util.mysql.MySQLDSL;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JooqMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        DSLContext ctxt = context.getBean(DSLContext.class);
-        ZoneOffset offset = fetchMySqlTimezoneOffset(ctxt);
-        System.err.println(offset);
+        clickhouseJooqTest(context);
+        //DSLContext ctxt = context.getBean(DSLContext.class);
+        //ZoneOffset offset = fetchMySqlTimezoneOffset(ctxt);
+        //System.err.println(offset);
+    }
+
+    public static void clickhouseJooqTest(ApplicationContext ctx) {
+        DataSource ds = ctx.getBean("clickhouseDatasource", DataSource.class);
+        DSLContext dslContext = new DefaultDSLContext(ds, SQLDialect.DEFAULT,
+                new Settings()
+                        .withRenderNameStyle(RenderNameStyle.AS_IS));
+        Field<Integer> dummyField = DSL.field("dummy", Integer.class);
+        Name systemOneTable = DSL.name("system.one");
+        dslContext.select(dummyField)
+                .from(systemOneTable)
+                .where(dummyField.eq(DSL.value(0)))
+                .execute();
     }
 
     public static ZoneOffset fetchMySqlTimezoneOffset0(DSLContext ctxt) {
