2aa83002b2244870c52f2c9561c8d5dac5d7a2fd
vladimir <smagellan@gmail.com>
2021-07-10 13:55:14 +0300

    updated pom.xml, moved log-collector

--- src/main/java/smagellan/test/logcollector/Constants.java
+++
@@ -1,8 +0,0 @@
-package smagellan.test.logcollector;
-
-public class Constants {
-    public static final String TAILED_AND_ROLLED_FILES_CHANNEL = "tailedAndRolledFilesChannel";
-    public static final String ROLLED_LOGS_IMPORT_CHANNEL = "rolledLogsImportChannel";
-    public static final String CONTROL_BUS = "controlBus";
-    public static final String FILE_TAILER_IDS = "fileTailerIds";
-}
