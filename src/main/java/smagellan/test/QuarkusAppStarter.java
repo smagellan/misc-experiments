23db113ab047eafb0032d9607bd60525b854ee4e
vladimir <smagellan@gmail.com>
2022-02-25 20:48:39 +0300

    add QuarkusAppStarter

---
+++ src/main/java/smagellan/test/QuarkusAppStarter.java
@@ -0,0 +1,20 @@
+package smagellan.test;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+import java.util.Arrays;
+
+@QuarkusMain
+public class QuarkusAppStarter implements QuarkusApplication {
+    public static void main(String[] args) {
+        Quarkus.run(QuarkusAppStarter.class, args);
+    }
+
+    @Override
+    public int run(String... args) throws Exception {
+        System.out.println("Hello " + Arrays.asList(args));
+        return 0;
+    }
+}
